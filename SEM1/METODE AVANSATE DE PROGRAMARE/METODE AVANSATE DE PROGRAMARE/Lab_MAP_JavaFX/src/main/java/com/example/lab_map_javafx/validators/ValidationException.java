248dfac6f8f33cde70cc62ad31596fd740686705
Vincze Iulia-Paula <93869285+V-IP@users.noreply.github.com>
2023-03-25 22:57:49 +0200

    update

--- SEM1/METODE AVANSATE DE PROGRAMARE/METODE AVANSATE DE PROGRAMARE/Lab_MAP_JavaFX/src/main/java/com/example/lab_map_javafx/validators/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.example.lab_map_javafx.validators;
-
-public class ValidationException extends RuntimeException {
-
-    public ValidationException(String message) {
-        super(message);
-    }
-}
--- SEM1/METODE AVANSATE DE PROGRAMARE/METODE AVANSATE DE PROGRAMARE/Lab_MAP_JavaFX/src/main/java/com/example/lab_map_javafx/validators/Validator.java
+++
@@ -1,5 +0,0 @@
-package com.example.lab_map_javafx.validators;
-
-public interface Validator<E> {
-    void validate(E entity) throws ValidationException;
-}
